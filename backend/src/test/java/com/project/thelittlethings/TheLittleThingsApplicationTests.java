7c3e6ec17956fa6f84800261ba5b3b34fbe1263a
nas0404 <naseem.win@hotmail.com>
2025-08-23 21:22:14 +1000

    Initial commit: add frontend + backend setup

---
+++ backend/src/test/java/com/project/thelittlethings/TheLittleThingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.thelittlethings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheLittleThingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
