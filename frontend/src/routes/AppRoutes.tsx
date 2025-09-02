3f40ba871e6667ec7f291daf5db9bbd8b6e36903
Ali-a-Idrees <ali.amiridrees@gmail.com>
2025-09-02 13:19:16 +1000

    Edited App route to make landing page - Landing

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -3,11 +3,13 @@ import Home from '../pages/Home';
 import About from '../pages/About';
 import Settings from '../pages/Settings';
 import NotFound from '../pages/NotFound';
+import Landing from '../pages/Landing';
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Landing />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/settings" element={<Settings />} />
       <Route path="*" element={<NotFound />} />
