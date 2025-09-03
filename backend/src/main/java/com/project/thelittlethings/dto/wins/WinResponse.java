90c489e619e435f2f15a2827df75190cb4210b3b
RecorderWow <pakenilker@hotmail.com>
2025-09-04 07:14:30 +1000

    wins logic implemented

--- backend/src/main/java/com/project/thelittlethings/dto/wins/WinResponse.java
+++ backend/src/main/java/com/project/thelittlethings/dto/wins/WinResponse.java
@@ -1,5 +1,20 @@
 package com.project.thelittlethings.dto.wins;
 
-public class WinResponse {
+import java.time.OffsetDateTime;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class WinResponse {
+    private Long winId;
+    private Long userId;
+    private Long goalId;
+    private String title;
+    private String description;
+    private Integer numTrophies;
+    private OffsetDateTime completionDate;
+    private Long journalId;
 }
