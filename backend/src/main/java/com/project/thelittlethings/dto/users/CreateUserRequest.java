975b2fdef5773328f38cff30ba762f0562a8179b
Ali-a-Idrees <ali.amiridrees@gmail.com>
2025-09-21 16:12:41 +1000

    added and updated DTO for user functions

--- backend/src/main/java/com/project/thelittlethings/dto/users/CreateUserRequest.java
+++ backend/src/main/java/com/project/thelittlethings/dto/users/CreateUserRequest.java
@@ -1,5 +1,39 @@
 package com.project.thelittlethings.dto.users;
 
-public class CreateUserRequest {
+import java.time.LocalDate;
 
+public class CreateUserRequest {
+	private String username;
+	private String email;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private LocalDate dob;
+	private String gender;
+	private String region;
+
+	// getters and setters
+	public String getUsername() { return username; }
+	public void setUsername(String username) { this.username = username; }
+
+	public String getEmail() { return email; }
+	public void setEmail(String email) { this.email = email; }
+
+	public String getPassword() { return password; }
+	public void setPassword(String password) { this.password = password; }
+
+	public String getFirstName() { return firstName; }
+	public void setFirstName(String firstName) { this.firstName = firstName; }
+
+	public String getLastName() { return lastName; }
+	public void setLastName(String lastName) { this.lastName = lastName; }
+
+	public LocalDate getDob() { return dob; }
+	public void setDob(LocalDate dob) { this.dob = dob; }
+
+	public String getGender() { return gender; }
+	public void setGender(String gender) { this.gender = gender; }
+
+	public String getRegion() { return region; }
+	public void setRegion(String region) { this.region = region; }
 }
