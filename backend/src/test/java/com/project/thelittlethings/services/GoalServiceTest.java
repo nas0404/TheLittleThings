11fabe6ce766c48f04e2839a417c5900fd4592c6
RecorderWow <pakenilker@hotmail.com>
2025-09-28 13:12:42 +1000

    goal service fix

--- backend/src/test/java/com/project/thelittlethings/services/GoalServiceTest.java
+++ backend/src/test/java/com/project/thelittlethings/services/GoalServiceTest.java
@@ -1,5 +1,6 @@
 // src/test/java/com/project/thelittlethings/services/GoalServiceTest.java
 package com.project.thelittlethings.services;
+
 import com.project.thelittlethings.dto.goals.CreateGoalRequest;
 import com.project.thelittlethings.dto.goals.GoalResponse;
 import com.project.thelittlethings.dto.goals.UpdateGoalRequest;
@@ -9,6 +10,7 @@ import com.project.thelittlethings.entities.User;
 import com.project.thelittlethings.repositories.CategoryRepository;
 import com.project.thelittlethings.repositories.GoalRepository;
 import com.project.thelittlethings.repositories.UserRepository;
+import com.project.thelittlethings.repositories.WinRepository;
 import com.project.thelittlethings.services.GoalService;
 
 import org.junit.jupiter.api.*;
@@ -22,24 +24,31 @@ import static org.mockito.Mockito.*;
 
 class GoalServiceTest {
 
-  @Mock GoalRepository goalRepo;
-  @Mock UserRepository userRepo;
-  @Mock CategoryRepository categoryRepo;
-
+  @Mock
+  GoalRepository goalRepo;
+  @Mock
+  UserRepository userRepo;
+  @Mock
+  CategoryRepository categoryRepo;
+  @Mock
+  WinRepository winRepo;
   GoalService service;
 
   @BeforeEach
   void setUp() {
     MockitoAnnotations.openMocks(this);
-    service = new GoalService(goalRepo, userRepo, categoryRepo);
+    service = new GoalService(goalRepo, userRepo, categoryRepo, winRepo);
   }
 
   @Test
   void create_success() {
     Long userId = 31L, catId = 4L;
 
-    var user = new User(); user.setUserId(userId);
-    var cat = new Category(); cat.setCategoryId(catId); cat.setUser(user);
+    var user = new User();
+    user.setUserId(userId);
+    var cat = new Category();
+    cat.setCategoryId(catId);
+    cat.setUser(user);
 
     when(userRepo.findById(userId)).thenReturn(Optional.of(user));
     when(categoryRepo.findById(catId)).thenReturn(Optional.of(cat));
@@ -89,10 +98,14 @@ class GoalServiceTest {
   @Test
   void create_categoryWrongOwner_400() {
     Long userId = 31L, catId = 4L;
-    var user = new User(); user.setUserId(userId);
+    var user = new User();
+    user.setUserId(userId);
 
-    var otherUser = new User(); otherUser.setUserId(99L);
-    var cat = new Category(); cat.setCategoryId(catId); cat.setUser(otherUser);
+    var otherUser = new User();
+    otherUser.setUserId(99L);
+    var cat = new Category();
+    cat.setCategoryId(catId);
+    cat.setUser(otherUser);
 
     when(userRepo.findById(userId)).thenReturn(Optional.of(user));
     when(categoryRepo.findById(catId)).thenReturn(Optional.of(cat));
@@ -106,11 +119,24 @@ class GoalServiceTest {
   void listByUser_success() {
     when(userRepo.existsById(31L)).thenReturn(true);
 
-    var user = new User(); user.setUserId(31L);
-    var cat = new Category(); cat.setCategoryId(4L); cat.setUser(user);
+    var user = new User();
+    user.setUserId(31L);
+    var cat = new Category();
+    cat.setCategoryId(4L);
+    cat.setUser(user);
 
-    var g1 = new Goal(); g1.setGoalId(1L); g1.setUser(user); g1.setCategory(cat); g1.setTitle("A"); g1.setPriority("HIGH");
-    var g2 = new Goal(); g2.setGoalId(2L); g2.setUser(user); g2.setCategory(cat); g2.setTitle("B"); g2.setPriority("LOW");
+    var g1 = new Goal();
+    g1.setGoalId(1L);
+    g1.setUser(user);
+    g1.setCategory(cat);
+    g1.setTitle("A");
+    g1.setPriority("HIGH");
+    var g2 = new Goal();
+    g2.setGoalId(2L);
+    g2.setUser(user);
+    g2.setCategory(cat);
+    g2.setTitle("B");
+    g2.setPriority("LOW");
 
     when(goalRepo.findByUser_UserId(31L)).thenReturn(List.of(g1, g2));
 
@@ -121,11 +147,19 @@ class GoalServiceTest {
 
   @Test
   void getOwnedGoal_wrongOwner_404() {
-    var owner = new User(); owner.setUserId(31L);
-    var other = new User(); other.setUserId(77L);
-    var cat = new Category(); cat.setCategoryId(4L); cat.setUser(owner);
+    var owner = new User();
+    owner.setUserId(31L);
+    var other = new User();
+    other.setUserId(77L);
+    var cat = new Category();
+    cat.setCategoryId(4L);
+    cat.setUser(owner);
 
-    var goal = new Goal(); goal.setGoalId(9L); goal.setUser(owner); goal.setCategory(cat); goal.setTitle("t");
+    var goal = new Goal();
+    goal.setGoalId(9L);
+    goal.setUser(owner);
+    goal.setCategory(cat);
+    goal.setTitle("t");
     when(goalRepo.findById(9L)).thenReturn(Optional.of(goal));
 
     var ex = assertThrows(IllegalArgumentException.class, () -> service.getOwnedGoal(9L, 77L));
@@ -134,11 +168,18 @@ class GoalServiceTest {
 
   @Test
   void update_priorityValidation_400() {
-    var user = new User(); user.setUserId(31L);
-    var cat = new Category(); cat.setCategoryId(4L); cat.setUser(user);
+    var user = new User();
+    user.setUserId(31L);
+    var cat = new Category();
+    cat.setCategoryId(4L);
+    cat.setUser(user);
 
-    var goal = new Goal(); goal.setGoalId(5L); goal.setUser(user); goal.setCategory(cat);
-    goal.setTitle("Old"); goal.setPriority("LOW");
+    var goal = new Goal();
+    goal.setGoalId(5L);
+    goal.setUser(user);
+    goal.setCategory(cat);
+    goal.setTitle("Old");
+    goal.setPriority("LOW");
 
     when(goalRepo.findByGoalIdAndUser_UserId(5L, 31L)).thenReturn(Optional.of(goal));
 
@@ -149,9 +190,15 @@ class GoalServiceTest {
 
   @Test
   void delete_success() {
-    var user = new User(); user.setUserId(31L);
-    var cat = new Category(); cat.setCategoryId(4L); cat.setUser(user);
-    var goal = new Goal(); goal.setGoalId(7L); goal.setUser(user); goal.setCategory(cat);
+    var user = new User();
+    user.setUserId(31L);
+    var cat = new Category();
+    cat.setCategoryId(4L);
+    cat.setUser(user);
+    var goal = new Goal();
+    goal.setGoalId(7L);
+    goal.setUser(user);
+    goal.setCategory(cat);
 
     when(goalRepo.findByGoalIdAndUser_UserId(7L, 31L)).thenReturn(Optional.of(goal));
 
