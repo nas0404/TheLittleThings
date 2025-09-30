0360ba71acaf888c535a707d010b5cb91fb260e0
Ali-a-Idrees <ali.amiridrees@gmail.com>
2025-09-30 23:29:48 +1000

    fixed issue with leaderBoardService

--- backend/src/main/java/com/project/thelittlethings/repositories/UserRepository.java
+++ backend/src/main/java/com/project/thelittlethings/repositories/UserRepository.java
@@ -1,8 +1,10 @@
 package com.project.thelittlethings.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.project.thelittlethings.entities.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -12,4 +14,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByUserId(long userId);
   boolean existsByUsername(String username);
   boolean existsByEmail(String email);
+
+  List<User> findByRegionOrderByTrophiesDesc(String region, Pageable pageable);
+
+  List<User> findAllByOrderByTrophiesDesc(Pageable pageable);
 }
