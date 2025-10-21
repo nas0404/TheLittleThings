4f20b0fd8f3e30f900007ad925cee7e96481af63
nas0404 <naseem.win@hotmail.com>
2025-10-21 11:30:28 +1100

    s

---
+++ backend/src/main/java/com/project/thelittlethings/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.project.thelittlethings.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "OK - The Little Things is running!";
+    }
+}
